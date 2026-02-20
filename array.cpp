aa22cc62ccf8c1eb14ef0f82d64a726395e8c251
vatsanshika923-prog <vatsanshika923@gmail.com>
2026-02-20 21:40:59 +0530

    Add files via upload

--- array.cpp
+++ array.cpp
@@ -185,4 +185,128 @@ int main() {
 
     return 0;
 }
+//maximum subaaray brute force aproach
+#include <iostream>
+#include <vector>
+using namespace std;
 
+int main(){
+     int n=5;
+     int arr[5]={1,2,3,4,5};
+      for(int st=0; st<n;st++){
+         for(int end=st;end<n; end++) {
+          for(int i=st; i<=end; i++){
+               cout<< arr[i];
+          }cout<<" ";
+         }
+         cout<<endl;
+     }
+     return 0;
+     
+} 
+//max subarray sum by brute force approach
+include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+int main(){
+     int n=5;
+     int arr[5]={1,2,3,4,5};
+     int maxSum=INT_MIN;
+      for(int st=0; st<n;st++){
+          int currSum=0;
+         for(int end=st;end<n; end++) {
+          currSum += arr[end];
+          maxSum = max(currSum,maxSum);
+          
+         }
+         
+     }
+     cout<<"max subarray sum="<<maxSum<<endl;
+    
+     return maxSum;
+} 
+//pair sum brute force approach 
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+vector<int> pairSum(vector<int> nums, int target){
+     vector<int> ans;
+     int n =nums.size();
+     for (int i =0; i<n ; i++){
+          for (int j = i+1; j< n; j++){
+               if (nums[i]+ nums[j] == target){
+                    ans.push_back(i);
+                    ans.push_back(j);
+                    return ans;
+               }
+          }
+     }
+}
+int main(){
+     vector<int> nums ={2,7,11,15};
+     int target=13;
+     vector <int> ans =pairSum(nums , target);
+     cout<< ans[0]<<", "<< ans[1]<< endl;
+     return 0;
+} 
+//by optimal appraoch using 2 pointers to solve pair sums
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+vector<int> pairSum(vector<int> nums, int target){
+     vector<int> ans;
+     int n =nums.size();
+     int i=0, j=n-1;
+     while(i<j){
+          int pairSum =nums[i]+nums[j];
+          if (pairSum> target){
+               j--;
+          }else if(pairSum< target){
+               i++;
+          }else {
+               ans.push_back(i);
+               ans.push_back(j);
+               return ans;
+          }
+     }
+}
+int main(){
+     vector<int> nums ={2,7,11,15};
+     int target=26;
+     vector <int> ans =pairSum(nums , target);
+     cout<< ans[0]<<", "<< ans[1]<< endl;
+     return 0;
+} 
+
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+vector<int> pairSum(vector<int> nums, int target){
+     vector<int> ans;
+     int n =nums.size();
+     int i=0, j=n-1;
+     while(i<j){
+          int pairSum =nums[i]+nums[j];
+          if (pairSum> target){
+               j--;
+          }else if(pairSum< target){
+               i++;
+          }else {
+               ans.push_back(i);
+               ans.push_back(j);
+               return ans;
+          }
+     }
+}
+int main(){
+     vector<int> nums ={2,7,11,15};
+     int target=26;
+     vector <int> ans =pairSum(nums , target);
+     cout<< ans[0]<<", "<< ans[1]<< endl;
+     return 0;
+} 
